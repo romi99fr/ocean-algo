4a0c76c279df50805834dd5277a51974c18f511f
Joel Romia <64154013+romi99fr@users.noreply.github.com>
2022-08-25 16:00:42 +0200

    Script to execute all steps to detect pigs

---
+++ detect-inputs-pigs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+jq -r '.[]' <(echo $DIDS) | while read i; do
+
+ unzip -j /data/inputs/$i/0 -d /data/input/ #UNZIP INPUT DATA to /data/input/
+
+done
+
+wait
+
+python3 splitImage.py /data/input/
+
+wait
+
+python3 rotateImages.py 
+
+wait
+
+python detect.py --weights /model/bestPig.pt --source /data/input/ # run yolov5 python to detect images at /data/input
+
+wait
+
+cd /usr/src/app/runs/detect/ # Change workdir
+
+wait
+
+zip -r results.zip exp*/* # Zip the results
+
+wait
+
+mv results.zip /data/outputs/. # Move results to /data/outputs/. to get them uploaded
+
+wait
